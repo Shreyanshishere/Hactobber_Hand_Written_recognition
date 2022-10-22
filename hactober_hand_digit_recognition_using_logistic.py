a12a81d4c3838ae52812fc16322ab56370c4001b
SHREYANSH GUPTA <105097099+Shreyanshishere@users.noreply.github.com>
2022-10-23 01:04:42 +0530

    Update hactober_hand_digit_recognition_using_logistic.py

--- hactober_hand_digit_recognition_using_logistic.py
+++ hactober_hand_digit_recognition_using_logistic.py
@@ -6,31 +6,22 @@ mnist=fetch_openml('mnist_784')
   #1 Update X and Y values
   
   
-x=mnist.data.values
-y=mnist.target
+
 
 temp=x[95]
 temp.shape
 
 # 2 import matplotlib and show a digit
-import matplotlib.pyplot as plt
 
-plt.imshow(temp.reshape(28,28)) 
 
 y[95]
 
 #3 import the train test library
-from sklearn.model_selection import train_test_split
 
-xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=0.2,random_state=10)
 
 
 #4 import the logistic regression model
-from sklearn.linear_model import LogisticRegression
 
-from matplotlib.scale import LogisticTransform
-
-model=LogisticRegression(multi_class='multinomial',solver='saga')
 
 model.fit(xtrain,ytrain)
 
